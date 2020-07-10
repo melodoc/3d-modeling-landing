b7950e8cb08315023bc5c4b011a3efaa7508daed
melodoc <l_tih@ro.ru>
2020-07-10 18:25:10 +0300

    Add menu handler delegation

--- script/script.js
+++ script/script.js
@@ -45,31 +45,30 @@ window.addEventListener('DOMContentLoaded', () => {
         updateClock();
     };
 
-    countTimer('11 july 2020');
+    countTimer('12 july 2020');
 
     //Menu
 
     const toggleMenu = () => {
 
-        const btnMenu = document.querySelector('.menu'),
-            menu = document.querySelector('menu'),
-            closeBtn = document.querySelector('.close-btn'),
-            menuItems = menu.querySelectorAll('ul>li');
+        const menu = document.querySelector('menu');
 
         const handlerMenu = () => {
             menu.classList.toggle('active-menu');
-            /*  no animation
-            if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {
-                    menu.style.transform = `translate(0)`;
-                } else {
-                    menu.style.transform = `translate(-100%)`;
-                }
-                */
         };
 
-        btnMenu.addEventListener('click', handlerMenu);
-        closeBtn.addEventListener('click', handlerMenu);
-        menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+        document.body.addEventListener('click', () => {
+            const target = event.target;
+
+            const closestBtnMenu = target.closest('.menu'),
+                closestMenu = target.closest('menu'),
+                closestCloseBtn = target.closest('.close-btn'),
+                closestMenuItems = target.closest('.active-menu ul>li');
+
+            if (closestBtnMenu || closestMenu || closestCloseBtn || closestMenuItems) {
+                handlerMenu();
+            }
+        });
     };
 
     toggleMenu();
