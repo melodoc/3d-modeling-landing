ceeb371395a79351c4ea5427d2226a9086823947
melodoc <l_tih@ro.ru>
2020-07-16 01:38:44 +0300

    Add polyfills

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 'use strict';
 
+import 'nodelist-foreach-polyfill';
+import '@babel/polyfill';
+import 'formdata-polyfill';
+import 'es6-promise';
+import 'fetch-polyfill';
+
+import elementClosest from 'element-closest';
+elementClosest(window);
+
 import calc from './modules/calc';
 import changePhoto from './modules/changePhoto';
 import checkDigitsInput from './modules/checkDigitsInput';
